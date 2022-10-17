95ccec7e83b0ffd5bb5e1f6088c84a9cc6758ca8
Jack54s <jack_smith@qq.com>
2022-10-17 22:47:38 +0800

    actionbar

--- app/src/main/java/com/jackzone/simpletimer/MainActivity.kt
+++ app/src/main/java/com/jackzone/simpletimer/MainActivity.kt
@@ -2,10 +2,28 @@ package com.jackzone.simpletimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(tool_bar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.setting -> Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+
 }
